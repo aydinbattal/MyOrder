342b7e07580bbd1d0b0a73caf771e2ddb8d4fcdd
Aydin Battal <abattal35@gmail.com>
2021-09-29 15:33:31 -0400

    Initial Commit

---
+++ Aydin_MyOrder/Aydin_MyOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aydin_MyOrderApp.swift
+//  Aydin_MyOrder
+//
+//  Created by Aydin Battal on 2021-09-29.
+//
+
+import SwiftUI
+
+@main
+struct Aydin_MyOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
