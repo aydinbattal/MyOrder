48e30c45a4f6c6151fc0635afd236c289044dee7
Aydin Battal <abattal35@gmail.com>
2021-09-30 12:06:23 -0400

    Implemented placing an order and saving it to a list on FirstView

--- Aydin_MyOrder/FirstView.swift
+++ Aydin_MyOrder/FirstView.swift
@@ -7,9 +7,61 @@
 
 import SwiftUI
 
+enum Type: String {
+    case darkRoast
+    case originalBlend
+    case vanilla
+    
+    var id: String { self.rawValue }
+}
+
+enum Size: String {
+    case small
+    case medium
+    case large
+    
+    var id: String { self.rawValue }
+}
+
 struct FirstView: View {
+    @State private var selectedType = Type.darkRoast
+    @State private var selectedSize = Size.small
+    @State private var tfQuantity : String = ""
+    @State private var orderList = [Order]()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+        VStack{
+            Text("Type:")
+            Picker("Type", selection: $selectedType) {
+                Text("Dark Roast").tag(Type.darkRoast)
+                Text("Original Blend").tag(Type.originalBlend)
+                Text("Vanilla").tag(Type.vanilla)
+            }
+            Text("Selected Type: \(selectedType.rawValue)")
+            
+            Picker("Size", selection: $selectedSize) {
+                Text("Small").tag(Size.small)
+                Text("Medium").tag(Size.medium)
+                Text("Large").tag(Size.large)
+            }
+            Text("Selected Size: \(selectedSize.rawValue)")
+            
+            TextField("Enter Quantity", text: $tfQuantity).keyboardType(.numberPad)
+            
+            Button(action: {
+                let quantity = Int(tfQuantity)
+                let currentOrder = Order(type: selectedType.rawValue, size: selectedSize.rawValue, quantity: quantity!)
+                self.orderList.append(currentOrder)
+                print(orderList)
+            }){
+                Text("Place Order")
+            }
+        }
+        .navigationBarItems(trailing: Button(action: {}){
+                                Text("My Orders")
+                                Image(systemName: "list.bullet.rectangle.portrait")})
+        }
     }
 }
 
